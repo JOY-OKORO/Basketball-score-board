014714dfd54e5d38730cf302c74c747b221c1ea2
The Female Techie <revolutionbrand18@gmail.com>
2025-03-02 14:05:24 +0100

    git commit-m

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-let add = (a, b) => a + b;
+
+let num = 0
+
+function add1() {
+     num += 1
+     document.getElementById("num").textContent = num;
+}
+
+function add2() {
+    num += 2
+    document.getElementById("num").textContent = num;
+}
+
+function add3() {
+    num += 3
+    document.getElementById("num").textContent = num;
+}
+
+let count = 0
+function add1() {
+    count += 1
+    document.getElementById("count").textContent = num;
+}
+
+function add2() {
+    count += 2
+    document.getElementById("count").textContent = num;
+}
+
+function add3() {
+    count += 3
+    document.getElementById("count").textContent = num;
+}
+
+function subtract1() {
+     count -= 1
+     document.getElementById("num").textContent = num;
+}
+
